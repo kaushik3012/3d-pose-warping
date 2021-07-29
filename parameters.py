edff4613d70a8962d3a64c838288d1860d6381f7
kaushik3012 <73692082+kaushik3012@users.noreply.github.com>
2021-07-30 03:04:10 +0530

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -45,7 +45,9 @@ def init():
 
     # validation configuration
     params['with_valid'] = True  # if True, training is performed on train and valid and tb outputs are on test split
-    params['valid_count'] = 20  # number of samples validation is based on
+    params['valid_count'] = 256  # number of samples validation is based on
+
+    params['test_count'] = 20    #number of samples to perform test
 
     params['name'] = 'fash-3d_w-3d_p'  # name will be appended to both the checkpoint directory and the tebsorboard directory
 
